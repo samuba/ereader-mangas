f5339a7d319f2cff7ef58374fcc2d1ec72d31647
samuba <samuel.z.bach@gmail.com>
2023-01-02 19:26:37 +0100

    do not cache errors

--- src/routes/scraping/image/+server.ts
+++ src/routes/scraping/image/+server.ts
@@ -8,11 +8,16 @@ export const GET = (async ({ url }) => {
 	const urlParts = imageUrl.split('.');
 	const filetype = urlParts[urlParts.length - 1].toLowerCase();
 	const data = await fetch(imageUrl, { headers: { Referer: new URL(imageUrl).origin } });
+
+	if (!data.ok || !data.headers.get('Content-Type')?.includes('image')) {
+		throw fail(502, { msg: `Error getting image from ${imageUrl}.\n${await data.text()}` });
+	}
+
 	const cachedMonths = 1 * 30 * 24 * 60 * 60;
 	return new Response(data.body, {
 		headers: {
 			'Content-Type': `image/${filetype}`,
-			'Cache-Control': `max-age=${cachedMonths}, immutable`
-		}
+			'Cache-Control': `max-age=${cachedMonths}, immutable`,
+		},
 	});
 }) satisfies RequestHandler;
--- src/routes/scraping/page/+server.ts
+++ src/routes/scraping/page/+server.ts
@@ -7,11 +7,16 @@ export const GET = (async ({ url }) => {
 	const pageUrl = url.searchParams.get('url');
 	if (!pageUrl) throw fail(400);
 	const data = await fetch(pageUrl, { headers: { Referer: new URL(pageUrl).origin } });
+
+	if (!data.ok || !data.headers.get('Content-Type')?.includes('html')) {
+		throw fail(502, { msg: `Error getting page from ${pageUrl}.\n${await data.text()}` });
+	}
+
 	const cachedHours = 12 * 60 * 60;
 	return new Response(data.body, {
 		headers: {
 			'Content-Type': `text/html`,
-			'Cache-Control': `max-age=${cachedHours}, immutable`
-		}
+			'Cache-Control': `max-age=${cachedHours}, immutable`,
+		},
 	});
 }) satisfies RequestHandler;
