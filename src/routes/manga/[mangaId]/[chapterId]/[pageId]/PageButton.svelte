ea5fa37381e8e5d9b0074f9990596147d469d4d9
samuba <samuel.z.bach@gmail.com>
2023-02-24 14:49:14 +0100

    style

--- src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
+++ src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
@@ -18,7 +18,7 @@
 	}
 </script>
 
-<div style="display: inline-block; margin-right: 1rem; margin-left: 1rem;">
+<div class="wrapper-ereader" style="display: inline-block;">
 	{#if url}
 		<a href={url} {title}>
 			<button class={$$props.class + ' button-ereader'}>
@@ -33,12 +33,20 @@
 </div>
 
 <style>
+	.wrapper-ereader {
+		margin-right: 1rem;
+		margin-left: 1rem;
+	}
+	.wrapper-noereader {
+		margin-right: 0.5rem;
+		margin-left: 0.5rem;
+	}
+
 	.button-ereader {
 		font-size: 9pt;
 		padding-right: 1rem;
 		padding-left: 1rem;
 	}
-
 	.button-noereader {
 		margin-top: 1rem;
 		margin-bottom: 0;
