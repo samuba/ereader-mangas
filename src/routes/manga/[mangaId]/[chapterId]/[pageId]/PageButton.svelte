7c2f779e64e655bd416355d66fc85deed7d49564
samuba <samuel.z.bach@gmail.com>
2023-02-24 14:45:56 +0100

    style

--- src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
+++ src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
@@ -1,26 +1,46 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let url: string | undefined;
 	export let title: string;
+
+	onMount(() => {
+		switchAllClassesToNoEreader(); // onMount does not get executed on kindle
+	});
+
+	function switchAllClassesToNoEreader() {
+		const all = document.getElementsByTagName('*');
+		for (var i = 0, max = all.length; i < max; i++) {
+			const element = all[i];
+			if (!element.className) continue;
+			element.className = element.className.replace(/-ereader/g, '-noereader');
+		}
+	}
 </script>
 
 <div style="display: inline-block; margin-right: 1rem; margin-left: 1rem;">
 	{#if url}
 		<a href={url} {title}>
-			<button class={$$props.class + ' butt'}>
+			<button class={$$props.class + ' button-ereader'}>
 				<slot />
 			</button>
 		</a>
 	{:else}
-		<button disabled class={$$props.class + ' butt'} title={`${title} not available`}>
+		<button disabled class={$$props.class + ' button-ereader'} title={`${title} not available`}>
 			<slot />
 		</button>
 	{/if}
 </div>
 
 <style>
-	.butt {
+	.button-ereader {
 		font-size: 9pt;
 		padding-right: 1rem;
 		padding-left: 1rem;
+	}
+
+	.button-noereader {
+		margin-top: 1rem;
+		margin-bottom: 0;
 	}
 </style>
