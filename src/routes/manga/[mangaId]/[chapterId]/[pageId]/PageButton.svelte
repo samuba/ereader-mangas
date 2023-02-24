d1c02a69b48e006914355d74e31ec5cb66b24ddf
samuba <samuel.z.bach@gmail.com>
2023-02-24 13:12:12 +0100

    style

--- src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
+++ src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
@@ -6,13 +6,13 @@
 <div style="display: inline-block; margin-right: 0.5rem; margin-left: 0.5rem;">
 	{#if url}
 		<a href={url} {title}>
-			<button style="font-size: 10pt; padding-right: 0.5rem; padding-left: 0.5rem;" class={$$props.class}>
+			<button style="font-size: 10pt; padding-right: 1rem; padding-left: 1rem;" class={$$props.class}>
 				<slot />
 			</button>
 		</a>
 	{:else}
 		<button
-			style="font-size: 10pt; padding-right: 0.5rem; padding-left: 0.5rem;"
+			style="font-size: 10pt; padding-right: 1rem; padding-left: 1rem;"
 			class={$$props.class}
 			disabled
 			title={`${title} not available`}
@@ -21,3 +21,6 @@
 		</button>
 	{/if}
 </div>
+
+<style>
+</style>
