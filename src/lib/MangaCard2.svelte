3cfd21a0200bd0eb24c9fe8a2583506013c0c901
samuba <samuel.z.bach@gmail.com>
2023-02-25 14:57:56 +0100

    fix for kindle

--- src/lib/MangaCard2.svelte
+++ src/lib/MangaCard2.svelte
@@ -4,17 +4,17 @@
 	export let manga: Manga;
 </script>
 
-<li class="col-span-1 flex rounded-md shadow-sm">
-	<div
-		class="flex-shrink-0 flex items-center justify-center max-w-xs bg-gray-800 text-white text-sm font-medium rounded-l-md min-h-[13rem]"
-	>
+<li class="rounded-md shadow-sm inline-block m-2">
+	<div class="inline-block bg-gray-800 text-white text-sm font-medium rounded-l-md min-h-[13rem] max-h-[13rem]">
 		<a href={`/manga/${manga.mangaId}`}>
-			<img src={manga.thumbnail} style="max-width: 140px;" class="rounded-l-md" alt="" />
+			<img src={manga.thumbnail} style="max-width: 140px;" class="rounded-l-md max-h-[13rem]" alt="" />
 		</a>
 	</div>
-	<div class="flex flex-1 items-center justify-between truncate rounded-r-md border-t border-r border-b border-gray-200 bg-white">
-		<div class="flex-1 truncate px-4 py-2 text-sm">
-			<a href={`/manga/${manga.mangaId}`} class="font-medium text-gray-900 hover:text-gray-600 text-lg">{manga.title}</a>
+	<div class="inline-block min-h-[13rem] w-96 truncate rounded-r-md border-t border-r border-b border-gray-200 bg-white">
+		<div class="flex-1 px-4 py-2 text-sm">
+			<a href={`/manga/${manga.mangaId}`} class="font-medium text-gray-900 hover:text-gray-600 text-lg whitespace-normal"
+				>{manga.title}</a
+			>
 			<div>
 				{#each manga.authors as author, authorIndex}
 					<a href={`?search=${encodeURIComponent('authors:' + author)}`} data-sveltekit-reload>
@@ -25,7 +25,7 @@
 					{/if}
 				{/each}
 			</div>
-			<div>
+			<div class="whitespace-normal">
 				{#each manga.genres as genre, i}
 					<a href={`?search=${encodeURIComponent('genres:' + genre)}`} data-sveltekit-reload>{genre}</a>
 					{#if i < manga.genres.length - 1}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
 </NavMenu>
 
 <div class="px-5">
-	<ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
+	<ul role="list" class="mt-3 mx-auto max-w-6xl">
 		{#each data.searchResults as manga (manga.mangaId)}
 			<MangaCard2 {manga} />
 		{/each}
