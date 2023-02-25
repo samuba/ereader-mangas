35a6026b7f8384be970edd63d92fab70bc49bb61
samuba <samuel.z.bach@gmail.com>
2023-02-25 15:09:33 +0100

    fix for kindle

--- src/lib/MangaCard2.svelte
+++ src/lib/MangaCard2.svelte
@@ -7,12 +7,14 @@
 <li class="rounded-md shadow-sm inline-block m-2">
 	<div class="inline-block align-top bg-gray-800 text-white text-sm font-medium rounded-l-md min-h-[13rem] max-h-[13rem]">
 		<a href={`/manga/${manga.mangaId}`}>
-			<img src={manga.thumbnail} style="max-width: 140px;" class="rounded-l-md max-h-[13rem]" alt="" />
+			<img src={manga.thumbnail} class="rounded-l-md max-h-[13rem] max-w-[20rem]" alt="" />
 		</a>
 	</div>
 	<div class="inline-block align-top min-h-[13rem] w-96 rounded-r-md border-t border-r border-b border-gray-200 bg-white">
 		<div class="px-4 py-2 text-sm">
-			<a href={`/manga/${manga.mangaId}`} class="font-medium text-gray-900 text-lg">{manga.title}</a>
+			<a href={`/manga/${manga.mangaId}`} class="font-medium text-gray-900 text-lg">
+				{manga.title}
+			</a>
 			<div>
 				{#each manga.authors as author, authorIndex}
 					<a href={`?search=${encodeURIComponent('authors:' + author)}`} data-sveltekit-reload>
