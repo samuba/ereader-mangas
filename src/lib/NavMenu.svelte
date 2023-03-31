0ddcd277f9133c3b7e8344788218d12e80a04529
samuba <samuel.z.bach@gmail.com>
2023-03-31 16:49:28 +0200

    colors

--- src/lib/NavMenu.svelte
+++ src/lib/NavMenu.svelte
@@ -7,14 +7,14 @@
 	{#if $$slots['search-input']}
 		<div class="mx-auto w-96 w-68 px-2 sm:px-4 lg:px-8 h-16 py-3">
 			<a href="/">
-				<div class="inline-block align-middle text-indigo-300 mr-6">
+				<div class="inline-block align-middle text-indigo-200 mr-6">
 					<AppIcon class="h-8 w-8 inline-block" />
 				</div>
 			</a>
 			<div class="inline-block align-middle w-[16rem]">
 				<div class="w-full">
 					<div class="relative">
-						<div class="pointer-events-none absolute top-2 left-0  pl-3">
+						<div class="pointer-events-none absolute top-2 left-0 pl-3">
 							<SearchIcon class="h-5 w-5 text-indigo-900" />
 						</div>
 						<slot name="search-input" />
@@ -25,7 +25,7 @@
 	{:else}
 		<center class="h-16 py-4">
 			<a href="/">
-				<div class="inline-block align-middle text-indigo-300">
+				<div class="inline-block align-middle text-indigo-200">
 					<AppIcon class="h-8 w-8 inline-block" />
 					<h1 class="inline-block ml-4 text-lg">eReader-Mangas</h1>
 				</div>
