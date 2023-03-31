9846ec1a05181d63b6aad4bcd6b75ec13a163c9b
samuba <samuel.z.bach@gmail.com>
2023-03-31 15:09:19 +0200

    styling

--- src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
+++ src/routes/manga/[mangaId]/[chapterId]/[pageId]/PageButton.svelte
@@ -3,27 +3,18 @@
 	export let title: string;
 </script>
 
-<div class="wrapper-ereader inline-block">
-	<a href={url} {title}>
-		<button class={$$props.class + ' button-ereader min-h-[1.5rem] min-w-[1.5rem]'}>
-			<slot />
-		</button>
-	</a>
-</div>
+<a href={url} {title} class="inline-block">
+	<button class={$$props.class + ' button-ereader min-h-[1.5rem] min-w-[1.5rem]'}>
+		<slot />
+	</button>
+</a>
 
 <style>
-	.wrapper-ereader {
-		/* margin-right: 1rem;
-		margin-left: 1rem; */
-	}
-	.wrapper-noereader {
-		/* margin-right: 0.5rem;
-		margin-left: 0.5rem; */
-	}
-
 	.button-ereader {
-		padding-right: 1rem;
-		padding-left: 1rem;
+		/* padding-right: 1rem;
+		padding-left: 1rem; */
+		margin-left: 1.5rem;
+		margin-right: 1.5rem;
 	}
 	.button-noereader {
 		font-size: 0.75rem;
