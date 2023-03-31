eaad19b32b3d5dcec28aa1f4e5e9782eb7a84a21
samuba <samuel.z.bach@gmail.com>
2023-03-31 17:08:15 +0200

    center spinner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import { fade } from 'svelte/transition';
 	import '../app.css';
 
-	let loadingNewPage = false;
+	let loadingNewPage = true;
 
 	if (browser) {
 		if (performance.navigation.type === 2) {
@@ -37,21 +37,14 @@
 		class="bg-slate-700/50 z-50 h-full w-full absolute flex justify-center items-center backdrop-blur-[3px]"
 		in:fade={{ delay: 300, duration: 200 }}
 	>
-		<div class="  text-white h-28 w-2 rounded-full">
-			<svg
-				class="animate-spin -ml-1 mr-3 h-16 w-16 text-white"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-			>
-				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
-				<path
-					class="opacity-75"
-					fill="currentColor"
-					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-				/>
-			</svg>
-		</div>
+		<svg class="animate-spin h-16 w-16 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+			<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+			<path
+				class="opacity-75"
+				fill="currentColor"
+				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+			/>
+		</svg>
 	</div>
 {/if}
 
