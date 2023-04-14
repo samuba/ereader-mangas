6311bcbdd7e7c72f39cd5130ed719cde16e0991c
samuba <samuel.z.bach@gmail.com>
2023-04-14 11:09:17 +0200

    use cloudflare workers for img proxying

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -3,6 +3,8 @@ export const routes = {
 	manga: (mangaId: string) => `/manga/${mangaId}`,
 	readPage: (mangaId: string, chapterId: string, pageId: string) => `/manga/${mangaId}/${chapterId}/${pageId}`,
 	scrapeImage: (url: string, referer: string) =>
-		`/scraping/image?url=${encodeURIComponent(url)}&referer=${encodeURIComponent(referer)}`,
+		`https://img-proxy.szb.workers.dev/?url=${encodeURIComponent(url)}&referer=${encodeURIComponent(referer)}`,
 	scrapePage: (url: string) => `/scraping/page?url=${encodeURIComponent(url)}`,
 };
+//  "Access-Control-Allow-Origin": "*",
+//       "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
--- src/routes/scraping/image/+server.ts
+++ src/routes/scraping/image/+server.ts
@@ -1,24 +1,23 @@
-import type { RequestHandler } from './$types';
-import { fail } from '@sveltejs/kit';
+// this now lives on https://img-proxy.szb.workers.dev to reduce vercel's metered egress
 
-export const GET = (async ({ url }) => {
-	const imageUrl = url.searchParams.get('url');
-	const referer = url.searchParams.get('referer') ?? '';
-	console.log({ imageUrl, referer });
-	if (!imageUrl) throw fail(400);
-	const urlParts = imageUrl.split('.');
-	const filetype = urlParts[urlParts.length - 1].toLowerCase();
-	const data = await fetch(imageUrl, { headers: { Referer: referer } });
+// export const GET = (async ({ url }) => {
+// 	const imageUrl = url.searchParams.get('url');
+// 	const referer = url.searchParams.get('referer') ?? '';
+// 	console.log({ imageUrl, referer });
+// 	if (!imageUrl) throw fail(400);
+// 	const urlParts = imageUrl.split('.');
+// 	const filetype = urlParts[urlParts.length - 1].toLowerCase();
+// 	const data = await fetch(imageUrl, { headers: { Referer: referer } });
 
-	if (!data.ok || !data.headers.get('Content-Type')?.includes('image')) {
-		throw fail(502, { msg: `Error getting image from ${imageUrl}.\n${await data.text()}` });
-	}
+// 	if (!data.ok || !data.headers.get('Content-Type')?.includes('image')) {
+// 		throw fail(502, { msg: `Error getting image from ${imageUrl}.\n${await data.text()}` });
+// 	}
 
-	const cachedMonths = 1 * 30 * 24 * 60 * 60;
-	return new Response(data.body, {
-		headers: {
-			'Content-Type': `image/${filetype}`,
-			'Cache-Control': `max-age=${cachedMonths}, immutable`,
-		},
-	});
-}) satisfies RequestHandler;
+// 	const cachedMonths = 1 * 30 * 24 * 60 * 60;
+// 	return new Response(data.body, {
+// 		headers: {
+// 			'Content-Type': `image/${filetype}`,
+// 			'Cache-Control': `max-age=${cachedMonths}, immutable`,
+// 		},
+// 	});
+// }) satisfies RequestHandler;
